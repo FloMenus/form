a3925377d37e31d9d5215ca3813377de20adca41
FloMenus <florent.mns@gmail.com>
2022-07-18 14:28:32 +0200

    finished 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const reg = /^[\w\.]+@([\w-]+\.)+[\w-]{2,4}$/
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      rememberMe: false,
+      emailIsValid: false,
+      passwordIsValid: false,
+      isSubmitted: false,
+    };
+  }
+
+  handleEmailChange = (e) => {
+    this.setState({
+      email: e.target.value,
+      emailIsValid: (reg.test(e.target.value))
+    })
+    console.log(this.state.emailIsValid)
+  }
+
+  handlePasswordChange = (e) => {
+    this.setState({
+      password : e.target.value,
+      passwordIsValid: (e.target.value.length > 4)
+    })
+    console.log(this.state.passwordIsValid)
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.emailIsValid && this.state.passwordIsValid) {
+      this.setState({
+        isSubmitted: true
+      })
+    }
+    console.log (this.state.isSubmitted)
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <div className="success">{this.state.email}</div>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label for="inputEmail">Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              id="inputEmail"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+              onChange={this.handleEmailChange}
+            ></input>
+          </div>
+          <div className="form-group">
+            <label for="inputPassword">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="inputPassword"
+              placeholder="Password"
+              onChange={this.handlePasswordChange}
+            ></input>
+          </div>
+          <div className="form-check">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              id="exampleCheck1"
+            ></input>
+            <label className="form-check-label" for="exampleCheck1">
+              Remember Me
+            </label>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
